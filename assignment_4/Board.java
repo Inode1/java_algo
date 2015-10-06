41eb8b64fdb6210ca01419a5e3b9baf8694dd9db
Inode1 <makarichevivan@gmail.com>
2015-10-06 23:41:09 +0300

    Board class complite.

--- assignment_4/Board.java
+++ assignment_4/Board.java
@@ -1,9 +1,13 @@
+import java.util.Iterator;
+
 public class Board {
     private int[][] board;
     private int dimension;
     private int emptyBlock = 0;
+    private int hamming = 0;
+    private int manhattan = 0;
     public Board(int[][] blocks) {           // construct a board from an N-by-N array of blocks
-        if (blocks = null) {
+        if (blocks == null) {
             throw new java.lang.NullPointerException();
         }
         dimension = blocks.length;
@@ -15,6 +19,8 @@ public class Board {
                 }
             }
         }
+        hammingCalculate();
+        manhattanCalculate();
 
     }
                                             // (where blocks[i][j] = block in row i, column j)
@@ -22,7 +28,10 @@ public class Board {
         return dimension;
     }
     public int hamming() {                  // number of blocks out of place
-        int hamming = 0;
+        return hamming;
+    }
+
+    private void hammingCalculate() {
         for (int i = 0; i < dimension; ++i) {
             for (int j = 0; j < dimension; ++j) {
                 if (board[i][j] != 0 && board[i][j] != j + dimension * i + 1) {
@@ -33,11 +42,15 @@ public class Board {
     }
 
     public int manhattan() {                // sum of Manhattan distances between blocks and goal
-        int manhattan = 0;
+        return manhattan;
+   
+    }
+
+    private void manhattanCalculate() {
         for (int i = 0; i < dimension; ++i) {
             for (int j = 0; j < dimension; ++j) {
                 if (board[i][j] != 0) {
-                    int row = (board[i][j]) / dimension;
+                    int row = board[i][j] / dimension;
                     int column = (board[i][j] - 1) % dimension;
                     row -= i;
                     if (row < 0) {
@@ -50,19 +63,30 @@ public class Board {
                     manhattan += row + column;
                 }   
             }
-        }   
+        }
     }
 
     public boolean isGoal() {                // is this board the goal board?
-        return hamming() == 0;
+        return hamming == 0;
     }
 
     public Board twin() {                   // a board that is obtained by exchanging any pair of blocks
-/*        for (int i = 0; i < dimension; ++i) {
-            for (int j = 0; j < dimension; ++j) {
-                
+        Board temp = new Board(board);
+        int i = 0;
+        int j = 0;
+        while (true) {
+            if (temp.board[0][i] != 0) {
+                if (j != 0) {
+                    int element = temp.board[0][i];
+                    temp.board[0][i] = temp.board[0][j];
+                    temp.board[0][j] = element;
+                    break;
+                }
+                j = i;
             }
-        }*/
+            ++i;
+        }
+        return temp;
     }
 
     public boolean equals(Object y) {       // does this board equal y?
@@ -96,34 +120,63 @@ public class Board {
     }
 
     private class Neighbors implements Iterator<Board> {
-        int iterationSize;
-        private Neighbors {
-            int row = emptyBlock % dimension;
-            int column = emptyBlock / dimension;
-            if (row == 0 || row == dimension - 1) {
-                if (column == 0 || column == dimension - 1) {
-                    return 
-                }
+        private int iterationSize = 0;
+        private int row = emptyBlock / dimension;
+        private int column = emptyBlock % dimension;
+        private Board[] vectorBoardElement;
+        private Neighbors() {
+            vectorBoardElement = new Board[4];
+            // left element
+            if (column > 0) {
+                vectorBoardElement[iterationSize] = new Board(board);
+                vectorBoardElement[iterationSize].board[row][column] = board[row][column - 1];
+                vectorBoardElement[iterationSize].board[row][column - 1] = 0;
+                ++iterationSize;
+            }
+            // top element
+            if (row > 0) {
+                vectorBoardElement[iterationSize] = new Board(board);
+                vectorBoardElement[iterationSize].board[row][column] = board[row - 1][column];
+                vectorBoardElement[iterationSize].board[row - 1][column] = 0;
+                ++iterationSize;
+            }
+            // right element
+            if (column < dimension - 1) {
+                vectorBoardElement[iterationSize] = new Board(board);
+                vectorBoardElement[iterationSize].board[row][column] = board[row][column + 1];
+                vectorBoardElement[iterationSize].board[row][column + 1] = 0;
+                ++iterationSize;
+            }
+            // bottom element
+            if (row < dimension - 1) {
+                vectorBoardElement[iterationSize] = new Board(board);
+                vectorBoardElement[iterationSize].board[row][column] = board[row + 1][column];
+                vectorBoardElement[iterationSize].board[row + 1][column] = 0;
+                ++iterationSize;
             }
         }
-        boolean hasNext(){
-
+        public boolean hasNext(){
+            return iterationSize != 0;
         }
 
-        Board next() {
-            
+        public Board next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+            --iterationSize;
+            return vectorBoardElement[iterationSize];
         }
     }
 
     public String toString() {               // string representation of this board (in the output format specified below)
-        String result;
+        String result = "";
         for (int i = 0; i < dimension; ++i) {
             for (int j = 0; j < dimension; ++j) {
-                string += board[i][j] + " ";
+                result += board[i][j] + " ";
             }
-            string += "\n";
+            result += "\n";
         }        
-
+        return result;
     }
     //public static void main(String[] args) // unit tests (not graded)
 }
