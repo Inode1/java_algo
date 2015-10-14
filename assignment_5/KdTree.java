ecb4216f5840fd546272937d2c9a4f03b7a9b2c6
Inode1 <makarichevivan@gmail.com>
2015-10-14 23:38:34 +0300

    Build

--- assignment_5/KdTree.java
+++ assignment_5/KdTree.java
@@ -15,17 +15,78 @@ public class KdTree {
     
     public void insert(Point2D p) {              // add the point to the set (if it is not already in the set)
         checkPoint(p);
-        while (root != null) {
-            if (root.)
-        }        
+        Node start = root;
+        boolean vertical = true;
+        while (start != null) {
+            GoNext(start, vertical);
+        }
+        start = new Node(new Point2D(p.x(), p.y()));
+        ++size;       
     }
     
     public boolean contains(Point2D p) {            // does the set contain point p? 
-    
+        checkPoint(p);
+        Node start = root;
+
+        while (start != null) 
+        {
+            if (start.point.equals(p))
+            {
+                break;
+            }
+            GoNext(start, vertical);
+        }
+        return start != null;
+    }
+
+    private void GoNext(Node next, boolean vertical) {
+        if (vertical)
+        {
+            if (start.compareForVertical(p) < 0) {
+                start = start.left;
+            }
+            else 
+            {
+                start = start.right;
+            }
+        }
+        else 
+        {
+            if (start.compareForHorizontal(p) < 0) {
+                start = start.left;
+            }
+            else 
+            {
+                start = start.right;
+            } 
+        }
+        vertical = !vertical;        
     }
     
     public void draw() {                         // draw all points to standard draw 
+        Node start = root;
+        StdDraw.setPenRadius(.01);
+        draw(start, true)
         
+    }
+
+    private void draw(Node next, boolean vertical) {
+        if (next == null) {
+            return;
+        }
+        StdDraw.setPenColor(StdDraw.BLACK);
+        next.point.draw();
+        if (vertical) {
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line
+        }
+        else
+        {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line
+        }
+        draw(next.left, !vertical);
+        draw(next.right, !vertical);
     }
     
     public Iterable<Point2D> range(RectHV rect) {             // all points that are inside the rectangle 
@@ -41,24 +102,20 @@ public class KdTree {
         private int maxLeftBorder;
         private Node right;
         private Node left;
+        public Node(Point2D point) {
+            this.point = point;
+        }
         public int compareForVertical(Point2D point) {
-            if (point.x() < this.point.x()) {
+            if (this.point.x() < point.x()) {
                 return -1;
-            }
-            if (point.x() > this.point.x()) {
-                return 1;
-            }
-            return -1;
+            return 1;
         }
 
         public int compareForHorizontal(Point2D point) {
-            if (point.x() < this.point.x()) {
+            if (this.point.y() < point.y()) {
                 return -1;
             }
-            if (point.x() > this.point.x()) {
-                return 1;
-            }
-            return -1;
+            return 1;
         }
     } 
 
