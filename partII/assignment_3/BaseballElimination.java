411ee69c73c6cffed47d0d4b9b2f0283c2e92ad6
ivan <makarichevivan@gmail.com>
2015-12-02 20:59:19 +0300

    trivial case

--- partII/assignment_3/BaseballElimination.java
+++ partII/assignment_3/BaseballElimination.java
@@ -1,6 +1,10 @@
 import java.util.Hashtable;
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.NavigableMap;
 import java.util.ArrayList;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.FlowEdge;
 import edu.princeton.cs.algs4.FlowNetwork;
 
@@ -12,11 +16,11 @@ public class BaseballElimination {
     private int[] remainingGames;
     private int[] gamesLeft;
     private int   size;
-    // max wins
-    private int   maxWins = 0;
     // internal data
     private FlowNetwork network;
     private ArrayList<Integer> eliminatedTeams;
+    // key wins, value team number
+    private TreeMap<Integer, Integer> sortedBestTeams = new TreeMap<Integer, Integer>();
 
     public BaseballElimination(String filename) {                    // create a baseball division from given filename in format specified below
         In file    = new In(filename);
@@ -32,9 +36,7 @@ public class BaseballElimination {
         while (i != size) {
             teams.put(file.readString(), i);
             wins[i]           = file.readInt();
-            if (wins[i] > maxWins) {
-                maxWins = wins[i];
-            }
+            sortedBestTeams.put(wins[i], i);
             losses[i]         = file.readInt();
             remainingGames[i] = file.readInt();
             for (int j = 0; j < size; ++j)
@@ -43,7 +45,6 @@ public class BaseballElimination {
             }
             ++i;
         }
-        eliminatedTeams = new ArrayList<Integer>(size);
         network = new FlowNetwork(size);
 
 
@@ -63,22 +64,21 @@ public class BaseballElimination {
     }
     
     public              int losses(String team) {                    // number of losses for given team
-        return losses[checkTeams(team)];;    
+        return losses[checkTeams(team)];    
     }
     
     public              int remaining(String team) {                 // number of remaining games for given team
-        return remaining[checkTeams(team)];;
+        return remainingGames[checkTeams(team)];
     }
     
     public              int against(String team1, String team2) {    // number of remaining games between team1 and team2
         int lhs = checkTeams(team1);
         int rhs = checkTeams(team2);
-        return wins[lhs * size + rhs];;
+        return wins[lhs * size + rhs];
     }
     
     public          boolean isEliminated(String team) {              // is given team eliminated?
-        
-        return true;
+        return checkTrivialCase(teams.get(team));
     }
     
     public Iterable<String> certificateOfElimination(String team) {  // subset R of teams that eliminates given team; null if not eliminated
@@ -93,15 +93,38 @@ public class BaseballElimination {
         return i;
     }
 
-    private void checkTrivialCase() {
-        for (int i = 0; i < size; ++i) {
-            if (wins[i] + remainingGames[i] < maxWins) {
-                eliminatedTeams.add(i);
-            }
+    private boolean checkTrivialCase(int teamNumber) {
+        NavigableMap<Integer, Integer> teamsIsBetter = sortedBestTeams.tailMap(wins[teamNumber] + remainingGames[teamNumber]
+                                                        , false);
+        if (teamsIsBetter.isEmpty()) {
+            return false;
         }
+        System.out.printf("team: %d\n", teamNumber);   
+        for(Map.Entry<Integer, Integer> entry: teamsIsBetter.entrySet())
+        {
+            System.out.println("teams: " + entry.getValue());
+        }
+/*        Iterator it = teamsIsBetter.iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+        }*/
+        return false;
     }
 
     public static void main(String[] args) {
-        BaseballElimination teams = new BaseballElimination(args[0]);
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team)) {
+                    StdOut.print(t + " ");
+                }
+                StdOut.println("}");
+            }
+            else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
     }
 }
