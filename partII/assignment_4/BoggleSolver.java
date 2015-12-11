fad14fcd8fba38f580604f2ea18da07f98551aab
ivan <makarichevivan@gmail.com>
2015-12-11 21:02:01 +0300

    need bonuse timeoout

--- partII/assignment_4/BoggleSolver.java
+++ partII/assignment_4/BoggleSolver.java
@@ -2,22 +2,25 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import java.util.Stack;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class BoggleSolver
 {
-    private Node root = new Node(null);
+    private Node root = new Node();
     private Stack<String> dict = new Stack<String>();
     private HashSet<String> result = new HashSet<String>();
     //private Stack<String> allFindPrefix = new Stack<String>();
     private int[] scourePoints = {0, 0, 0, 1, 1, 2, 3, 5, 11};
-    private ArrayList<ArrayList<Integer>> precompute;
+    private Adjacent adj; 
+    private HashMap<String, Adjacent> prec = new HashMap<String, Adjacent>();
+    private String onlyOne = "";
+    private BoggleBoard board;
+    private boolean[] alreadyBeing;
 
-    private int[][] precomputeArray;
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-        root.back = root;
         for (String word: dictionary) {
             Node iterator = root;
             boolean flag = true;
@@ -25,115 +28,62 @@ public class BoggleSolver
                 int symb = (int) word.charAt(i) - 65;
                 // Q
                 if (symb == 16) {
-                    if (i + 1 < word.length()) {
-                        if ((int) word.charAt(i + 1) - 65 == 20) {
-                            i += 1;
-                        }
-                    } else
-                    {
+                    if (i + 1 < word.length() && (int) word.charAt(i + 1) - 65 == 20) {
+                        i += 1;
+                    } else {
                         flag = false;
                         break;
                     }
                 }
                 if (iterator.next[symb] == null) {
-                    iterator.next[symb] = new Node(iterator);
+                    iterator.next[symb] = new Node();
                 }
                 iterator = iterator.next[symb];
             }
-            iterator.value = flag; 
+            if (flag) {
+                iterator.value = true; 
+            }
         }
-        //getAllWord(root, "");
+        for (int i = 1; i < 11; ++i) {
+            for (int j = 1; j < 11; ++j) {
+                String temp = "" + i + "x" + j; 
+                prec.put(temp, new Adjacent(i, j));
+            }
+        }
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
         result.clear();
-        precompute = new ArrayList<ArrayList<Integer>>(board.rows() * board.cols());
-        //precompute.addAll(new ArrayList<Integer>(8));
-        //precomputeArray = new int[board.rows() * board.cols()][9];
-        for (int i = 0; i < board.rows() * board.cols(); ++i) {
-            precompute.add(new ArrayList<Integer>(8));
+        String temp = "" + board.rows() + "x" + board.cols();
+        adj = prec.get(temp);
+        if (adj == null) {
+            System.out.println(temp);
+            adj = new Adjacent(board.rows(), board.cols());
+            prec.put(temp, adj);
         }
-        for (int i = 0; i < board.rows() - 1; ++i) {
+
+        this.board = board; 
+        Node position;
+        alreadyBeing = new boolean[board.rows() * board.cols()];
+        for (int i = 0; i < board.rows(); ++i) {
             for (int j = 0; j < board.cols(); ++j) {
-                int temp = i * board.cols() + j;
-                ArrayList<Integer> that = precompute.get(temp);
-                that.add((i + 1)* board.cols() + j);
-                precompute.get((i + 1)* board.cols() + j).add(temp);
-                if (j + 1 < board.cols()) {
-                    that.add(temp + 1);
-                    that.add((i + 1)* board.cols() + j + 1);
-                    precompute.get(i * board.cols() + j + 1).add(temp);
-                    precompute.get((i + 1)* board.cols() + j + 1).add(temp);                
+                position = root.next[board.getLetter(i, j) - 65];
+                if (position == null) {
+                    continue;
                 }
-                if (j > 0) {
-                    that.add((i + 1)* board.cols() + j - 1);
-                    precompute.get((i + 1)* board.cols() + j - 1).add(temp);
+                int p = i * board.cols() + j;
+                alreadyBeing[p] = true;
+                /*if (board.getLetter(i, j) == 'Q') {
+                    deepFirstSearch(position, i * board.cols() + j, "" + board.getLetter(i, j) + "U", alreadyBeing);
                 }
-
-/*                int p = 0;
-                boolean isGood = false;
-                int number = i * board.cols() + j;
-                if (i > 0) {
-                    precomputeArray[number][p++] = (i - 1) * board.cols() + j;
-                    if (i + 1 < board.rows()) {
-                        precomputeArray[number][p++] = (i + 1) * board.cols() + j;
-                        isGood = true;
-                    }
-                    if (j > 0) {
-                       precomputeArray[number][p++] = i * board.cols() + j - 1;
-                       if (isGood) {
-                        precomputeArray[number][p++] = (i + 1) * board.cols() + j - 1; 
-                       } 
-                       precomputeArray[number][p++] = (i - 1) * board.cols() + j - 1; 
-                    }
-                    if (j + 1 < board.cols()) {
-                       precomputeArray[number][p++] = i * board.cols() + j + 1;
-                       if (isGood) {
-                        precomputeArray[number][p++] = (i + 1) * board.cols() + j + 1; 
-                       } 
-                       precomputeArray[number][p++] = (i - 1) * board.cols() + j + 1; 
-                    }
-                }
-                else {
-                    if (i + 1 < board.rows()) {
-                        precomputeArray[number][p++] = (i + 1) * board.cols() + j;
-                        isGood = true;
-                    }
-                    if (j > 0) {
-                       precomputeArray[number][p++] = i * board.cols() + j - 1;
-                       if (isGood) {
-                            precomputeArray[number][p++] = (i + 1) * board.cols() + j - 1; 
-                       }
-                    }
-                    if (j + 1 < board.cols()) {
-                       precomputeArray[number][p++] = i * board.cols() + j + 1;
-                       if (isGood) {
-                            precomputeArray[number][p++] = (i + 1) * board.cols() + j + 1; 
-                       } 
-                    }
-                }
-                precomputeArray[number][p] = -1;*/
+                else
+                {
+                }*/
+                    deepFirstSearch(position, p);
+                alreadyBeing[p] = false;
             }
         }
-
-        for (int i = board.rows() - 1; i < board.rows(); ++i) {
-            for (int j = 0; j < board.cols() - 1; ++j) {
-                int temp = i * board.cols() + j;
-                precompute.get(temp).add(temp + 1);
-                precompute.get(temp + 1).add(temp);
-            }
-        }
-
-/*        for (int i = 0; i < board.rows(); ++i) {
-            for (int j = 0; j < board.cols(); ++j) {
-                for (int k: precompute.get(i * board.cols() + j)) {
-                    System.out.print(k + " ");
-                }
-                System.out.println();
-            }
-        }*/
-        deepFirstSearch(board);
         return result;
     }
 
@@ -152,12 +102,58 @@ public class BoggleSolver
 
     private class Node {
         private static final int RADIX = 26;
-        private Node(Node back) {
-            this.back = back;
-        }
         private boolean value;
         private Node[] next = new Node[RADIX];
-        private Node back; 
+    }
+
+    private class Adjacent {
+        private int[][] adj;
+        private int[] capacity;
+        private int[] position;
+        private Adjacent(int row, int column) {
+            adj = new int[row * column][8];
+            capacity = new int[row * column];
+            position = new int[row * column];
+            for (int i = 0; i < row - 1; ++i) {
+                for (int j = 0; j < column; ++j) {
+                    int temp = i * column + j;
+                    add(temp, (i + 1) * column + j);
+                    if (j + 1 < column) {
+                        add(temp, temp + 1);
+                        add(temp, (i + 1) * column + j + 1);
+                    }
+                    if (j > 0) {
+                        add(temp, (i + 1) * column + j - 1);
+                    }
+                }
+            }
+            for (int i = row - 1; i < row; ++i) {
+                for (int j = 0; j < column - 1; ++j) {
+                    add(i * column + j, i * column + j + 1);
+                }
+            }
+        }
+
+        private void add(int index, int value) {
+            adj[index][capacity[index]++] = value;
+            adj[value][capacity[value]++] = index;
+        }
+
+        // return -1 if non more elements
+        private int get(int index, int pos) {
+            /*if (capacity[index] == position[index]) {
+                return -1;
+            }*/
+            if (pos == capacity[index]) {
+                return -1;
+            }
+            return adj[index][pos];
+        }
+
+        private void clear(int index) {
+            position[index] = 0;
+        }
+
     }
 
     private void getAllWord(Node position, String prefix) {
@@ -176,6 +172,9 @@ public class BoggleSolver
         Node position = root;
         for (int i = 0; i < prefix.length(); ++i) {
             position = position.next[prefix.charAt(i) - 65];
+            if (prefix.charAt(i) == 'Q') {
+                ++i;    
+            }
             if (position == null) {
                 return false;
             }
@@ -183,7 +182,87 @@ public class BoggleSolver
         return position.value; 
     }
 
-    private void deepFirstSearch(BoggleBoard board) {
+/*    private void deepFirstSearch(BoggleBoard board) {
+        boolean[] alreadyBeing = new boolean[board.rows() * board.cols()];
+        //StringBuffer prefix = new StringBuffer(20);
+        
+        Stack<Integer> alreadyBeingUpdate = new Stack<Integer>();
+        for (int i = 0; i < board.rows(); ++i) {
+            for (int j = 0; j < board.cols(); ++j) {
+                //String prefix = "";
+                int start = i * board.cols() + j;
+                char symb = board.getLetter(i, j);
+                Node nodePositon = root.next[symb - 65];
+                    /*for (int z = 0; z < board.rows() * board.cols(); ++z) {
+                        //adj.clear(z);
+                        System.out.println(alreadyBeingUpdate.size());
+                        //alreadyBeing[z] = false;
+                    }*/
+
+                                    /*alreadyBeing[start] = true;
+                alreadyBeingUpdate.push(start);
+
+                if (nodePositon == null) {
+                    continue;
+                }
+                //prefix.append(symb);
+                /*prefix += symb;
+                if (symb == 'Q') {
+                    //prefix.append('U');
+                    prefix += 'U';
+                }
+                alreadyBeing[start] = true;
+                alreadyBeingUpdate.push(start);
+
+                while (nodePositon != root) {
+                    start = adj.get(start);
+                    if (start == -1) {
+                        start = alreadyBeingUpdate.pop();
+                        alreadyBeing[start] = false;
+                        nodePositon = nodePositon.back;
+                        adj.clear(start);
+                        if (nodePositon == root) {
+                            break;
+                        }
+                        /*if (board.getLetter(start / board.cols(), start % board.cols()) == 'Q') {
+                            //prefix.setLength(prefix.length() - 2);
+                            prefix = prefix.substring(0, prefix.length() - 2);
+                        } else
+                        {
+                            //prefix.setLength(prefix.length() - 1);
+                             prefix = prefix.substring(0, prefix.length() - 1);
+                        }
+                        start = alreadyBeingUpdate.peek();
+                        continue;
+                    }
+                    symb = board.getLetter(start / board.cols(), start % board.cols());
+                    if (alreadyBeing[start] == true || nodePositon.next[symb - 65] == null) {
+                        start = alreadyBeingUpdate.peek();
+                        continue;
+                    }
+                    if (nodePositon.next[symb - 65] == null) {
+                        start = alreadyBeingUpdate.peek();
+                        continue;
+                    }
+                    nodePositon = nodePositon.next[symb - 65];
+                    //prefix.append(symb);
+                    /*prefix += symb;
+                    if (symb == 'Q') {
+                        //prefix.append('U');
+                        prefix += 'U';
+                    }
+                    alreadyBeing[start] = true;
+                    alreadyBeingUpdate.push(start);
+                    /*if (nodePositon.value && prefix.length() >= 3) {
+                        result.add(prefix);
+                    }
+                }  
+            }
+        }
+    }
+*/
+
+/*    private void deepFirstSearch(BoggleBoard board) {
         Stack<String> prefixString = new Stack<String>();
         prefixString.ensureCapacity(1000); 
         Stack<Integer> alreadyBeingUpdate = new Stack<Integer>();
@@ -223,7 +302,7 @@ public class BoggleSolver
 
                     
                     if (sybvols == 'Q') {
-                        /*if (position.next['U' - 65] == null) {
+                        if (position.next['U' - 65] == null) {
                             int count1 = 0;
                             while (!prefixString.isEmpty() && prefixString.peek() == "") {
                                 alreadyBeing[alreadyBeingUpdate.pop()] = false;
@@ -236,7 +315,7 @@ public class BoggleSolver
                             }
                             continue;
                         }
-                        position = position.next['U' - 65];*/
+                        position = position.next['U' - 65];
                         prefix += 'U';
                     }
 
@@ -264,39 +343,34 @@ public class BoggleSolver
             }
         }
 
-    }
+    }*/
 
-/*    private void deepFirstSearch(Node position, int i, BoggleBoard board, String prefix, boolean[] alreadyBeing) {
-        alreadyBeing[i] = true;
-        int j = i / board.cols();
-        int z = i % board.cols();
-        position = position.next[board.getLetter(j, z) - 65];
-        if (position == null) {
-            return;
-        }
-        prefix += board.getLetter(j, z);
-        if (board.getLetter(j, z) == 'Q') {
-            position = position.next[(int) 'U' - 65];
-            if (position == null) {
-                return;
-            }
-            prefix += "U";
-        }
-        
-        if (position.value && prefix.length() >= 3) {
-            //dict.push(prefix.toString());
-            result.add(prefix);
-        }
+    private void deepFirstSearch(Node position, int i) {
+        /*if (position.value && prefix.length() >= 3) {
+                        result.add(prefix);
+        }*/
 
-        for (int k = 0; k < 8 && precomputeArray[i][k] != -1; ++k) {
-            int temp = precomputeArray[i][k];
-            if (alreadyBeing[temp] == true) {
+
+        for (int k = 0; k < 8 && adj.get(i, k) != -1; ++k) {
+            int temp = adj.get(i, k);
+            int j = temp / board.cols();
+            int z = temp % board.cols();
+            Node next;
+            if (alreadyBeing[temp] == true || (next = position.next[board.getLetter(j, z) - 65]) == null) {
                 continue;
             }
-            deepFirstSearch(position, temp, board, prefix, alreadyBeing);
+            alreadyBeing[temp] = true;
+            /*if (board.getLetter(j, z) == 'Q') {
+                deepFirstSearch(position.next[board.getLetter(j, z) - 65], temp, prefix + board.getLetter(j, z) + "U", alreadyBeing);
+            } else
+            {
+            }*/
+                deepFirstSearch(next, temp);
+            
+            
             alreadyBeing[temp] = false;
         }
-    }*/
+    }
 
     public static void main(String[] args) {
         In in = new In(args[0]);
